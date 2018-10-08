b5074e284a7e8e962500b7a076d73248c271c30e
Will Larche <willlarche@gmail.com>
2018-10-07 23:21:46 -0400

    [Model] Updated manager.

--- MVC/Model/CatManager.swift
+++ MVC/Model/CatManager.swift
@@ -6,45 +6,95 @@
 //  Copyright © 2018 Will Larche. All rights reserved.
 //
 
-import Foundation
+import Alamofire
+import SwiftyJSON
 
-enum ImageType {
-  case gif
-  case jpg
-}
+struct CatManager {
 
-enum CatSite {
-  case randomCat
-  case theCatAPI(ofType: ImageType)
+  private enum StringConstants {
 
-  var url: URL? {
-    var components = URLComponents()
-    components.scheme = "https"
+  }
 
-    switch self {
-    case .randomCat:
-      components.host = "aws.random.cat"
-      components.path = "meow"
-    case .theCatAPI(let ofType):
-      components.host = "api.thecatapi.com"
-      components.path = "v1/images/search"
-      components.queryItems = [URLQueryItem(name: "size", value: "small")]
+  private enum RandomCatStringConstants {
 
-      switch ofType {
-      case .gif:
-        components.queryItems? += [URLQueryItem(name: "mime_types", value: "gif")]
-      case .jpg:
-        components.queryItems? += [URLQueryItem(name: "mime_types", value: "jpg")]
+  }
+
+  private enum TheCatAPIStringConstants {
+
+  }
+
+  enum ImageType {
+    case gif
+    case jpg
+  }
+
+  enum CatSite {
+    case randomCat
+    case theCatAPI(ofType: ImageType)
+
+    var url: URL? {
+      var components = URLComponents()
+      components.scheme = "https"
+
+      switch self {
+      case .randomCat:
+        components.host = "aws.random.cat"
+        components.path = "/meow"
+      case .theCatAPI(let ofType):
+        components.host = "api.thecatapi.com"
+        components.path = "/v1/images/search"
+        components.queryItems = [URLQueryItem(name: "size", value: "small")]
+
+        switch ofType {
+        case .gif:
+          components.queryItems? += [URLQueryItem(name: "mime_types", value: "gif")]
+        case .jpg:
+          components.queryItems? += [URLQueryItem(name: "mime_types", value: "jpg")]
+        }
+      }
+      return components.url
+    }
+  }
+
+  enum CatFetchingError: Error {
+    case serverError
+    case emptyOrUnexpectedValue
+    case urlMissing
+  }
+
+  static func getCat(service site: CatSite,
+                     success: @escaping (Cat) -> Void,
+                     failure: @escaping (Error) -> Void) {
+    guard let url = site.url else {
+      failure(CatFetchingError.urlMissing)
+      return
+    }
+
+    fetchCatData(url: url, success: { (value) in
+      guard let value = value else {
+        failure(CatFetchingError.emptyOrUnexpectedValue)
+        return
+      }
+
+      let jsonObject = JSON(value)
+      let cat = Cat(identifier: jsonObject["file"].stringValue, url: jsonObject["file"].url)
+      success(cat)
+    }, failure: { (error) in
+
+    })
+
+  }
+
+  fileprivate static func fetchCatData(url: URL,
+                                       success: @escaping (Any?) -> Void,
+                                       failure: @escaping (Error?) -> Void) {
+    Alamofire.request(url).responseJSON { response in
+      if response.result.isSuccess {
+        success(response.result.value)
+      } else {
+        failure(response.result.error)
       }
     }
-    return components.url
   }
-}
 
-func fetchCat(service site: CatSite, type: ImageType) -> Cat {
-  return Cat(identifier: "", url: URL(string: ""))
-}
-
-func getImage() {
-  
 }
